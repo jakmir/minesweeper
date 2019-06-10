1f5490cece8af6bad7428fdecc032fa7a154d02e
Jakmir Insight <jakmir.net@gmail.com>
2019-06-10 12:34:30 +0300

    Layout modernization and further decoupling

---
+++ JapaneseMinesweeper/Extensions/UIView+MakeFitToEdges.h
@@ -0,0 +1,17 @@
+//
+//  UIView+MakeFitToEdges.h
+//  JapaneseMinesweeper
+//
+//  Created by Denis on 6/9/19.
+//  Copyright © 2019 Jakmir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (MakeFitToEdges)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JapaneseMinesweeper/Extensions/UIView+MakeFitToEdges.m
@@ -0,0 +1,13 @@
+//
+//  UIView+MakeFitToEdges.m
+//  JapaneseMinesweeper
+//
+//  Created by Denis on 6/9/19.
+//  Copyright © 2019 Jakmir. All rights reserved.
+//
+
+#import "UIView+MakeFitToEdges.h"
+
+@implementation UIView (MakeFitToEdges)
+
+@end
